55ae374d124cccee43852f6b20ffcd883baa41be
ksj <132119447+KSJ0314@users.noreply.github.com>
2025-02-16 17:14:53 +0900

    Update and rename 김소중.java to Solution.java

--- 5653. 줄기세포배양/Solution.java
+++ 5653. 줄기세포배양/Solution.java
@@ -9,10 +9,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class 김소중 {
+public class Solution {
 	public static void main(String[] args) throws IOException {
 		// System.setIn(new FileInputStream("./res/input.txt"));
-		김소중 s = new 김소중();
+		Solution s = new Solution();
 		s.init();
 		System.out.println(s.sb.toString());
 	}
